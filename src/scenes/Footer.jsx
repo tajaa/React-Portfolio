4a05c100c7e955f10bd5b4e842ec65bf0d1e39b8
tajaa <tajatheprince@gmail.com>
2023-06-13 12:39:11 -0700

    fifth commit: changing color schema

--- src/scenes/Footer.jsx
+++ src/scenes/Footer.jsx
@@ -2,14 +2,16 @@ import SocialMediaIcons from "../components/SocialMediaIcons";
 
 const Footer = () => {
   return (
-    <footer className="h-64 bg-red pt-10">
+    <footer className="h-64 bg-slate-500 pt-10">
       <div className="w-5/6 mx-auto">
         <SocialMediaIcons />
         <div className="md:flex font-playfair justify-center md:justify-between text-center">
-          <p className="font-playfair font-semibold text-2xl text-yellow">
+          <p className="font-playfair font-semibold text-2xl text-gray-100">
             Bobby Stone
           </p>
-          <p className="font-playfair text-md text-yellow">2022 ION-COMPUTE</p>
+          <p className="font-playfair text-md text-gray-100">
+            2022 ION-COMPUTE
+          </p>
         </div>
       </div>
     </footer>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
       colors: {
         "deep-blue": "#010026",
         blue: "#2CBCE9",
-        red: "#DC4492",
+        red: "#8b0000",
         yellow: "#FDCC49",
         grey: "#ededed",
         "dark-grey": "#757575",
